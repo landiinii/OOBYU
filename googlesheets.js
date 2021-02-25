e6d300b04f920aa4e07430895300cdd98231addd
landiinii <bailey.landeng@gmail.com>
2021-02-25 10:12:13 -0700

    added column to stats

--- googlesheets.js
+++ googlesheets.js
@@ -3,7 +3,7 @@ var visualization;
 
 function drawVisualization() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1NOy_h4USk9r2cMUiOQe6TXdgMzhHoSDNOYDr5x7PgsE/edit#gid=0');
-    query.setQuery('SELECT A, B, C, D, E, F label A "Date", B "New Contacts", C "New Cases", D "Total Cases", E "Total Deaths", F "Vaccinations"');
+    query.setQuery('SELECT A, B, C, D, E, F, G label A "Date", B "New Contacts", C "New Cases", D "Total Cases", E "Total Deaths", F "Total Recovered", G "Vaccinations"');
     query.send(handleQueryResponse);
 }
 
